8572945ab118a6510fa30f70023a8197a4734622
XinLei <1652219550a@gmail.com>
2018-09-19 11:53:27 +0800

    增加闪屏页demo

---
+++ mecury_project/example/splash_screen_demo/lib/splash_screen.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'home_page.dart';
+
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen>
+    with SingleTickerProviderStateMixin {
+  AnimationController _controller;
+  Animation _animation;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _controller =
+        AnimationController(vsync: this, duration: Duration(milliseconds: 1500));
+    _animation = Tween(begin: 0.0, end: 1.0).animate(_controller);
+
+    _animation.addStatusListener((status) {
+      if (status == AnimationStatus.completed) {
+        Navigator.of(context).pushAndRemoveUntil(
+            MaterialPageRoute(builder: (context) => MyHomePage(title: 'Splash Demo')),
+            (route) => route == null);
+      }
+    });
+    _controller.forward();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new FadeTransition(
+      opacity: _animation,
+      child: new Image.asset(
+        'assets/photo.jpg',
+        scale: 2.0,
+        fit: BoxFit.cover,
+      ),
+    );
+  }
+}
