15674068c09a5c8ff11e5b5148701e879d44cc88
XinLei <1652219550a@gmail.com>
2018-10-09 11:10:12 +0800

    修复animation demo
    修复正方形变成圆形的demo的border radius失效问题

--- mecury_project/example/animation_demo/lib/animation_demo/transforming_animation_demo.dart
+++ mecury_project/example/animation_demo/lib/animation_demo/transforming_animation_demo.dart
@@ -18,9 +18,9 @@ class _HomeScreenState extends State<HomeScreen> with SingleTickerProviderStateM
     animationController = AnimationController(vsync: this,duration:Duration(seconds: 5));
     animation = BorderRadiusTween(
         begin:BorderRadius.circular(0.0),
-        end: BorderRadius.circular(150.0)
+        end: BorderRadius.circular(120.0)
     ).animate(CurvedAnimation(parent: animationController, curve: Curves.ease));
-    animationController.forward();
+    animationController.repeat();
     super.initState();
   }
 
@@ -33,18 +33,22 @@ class _HomeScreenState extends State<HomeScreen> with SingleTickerProviderStateM
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: AnimatedBuilder(animation: animationController, builder: (BuildContext context, Widget child){
-        return Center(
-          child: Material(
-            borderRadius: animation.value,
+      body: Container(
+        color: Colors.amber,
+        child: AnimatedBuilder(animation: animationController, builder: (BuildContext context, Widget child){
+          return Center(
             child: Container(
-              color: Colors.lightBlue,
+              decoration: BoxDecoration(
+                borderRadius: animation.value,
+                color: Colors.blue
+              ),
               width: 200.0,
               height: 200.0,
+              child: Text(animation.value.toString()),
             ),
-          ),
-        );
-      }),
+          );
+        }),
+      ),
     );
   }
 }
