bc054f8c3d777799b800e8a07ea905652350ed51
XinLei <1652219550a@gmail.com>
2018-11-02 16:21:17 +0800

    Update mecury_project/example/animation_challenge/lib/animation_demo/audio_screen.dart

--- mecury_project/example/animation_challenge/lib/animation_demo/audio_screen.dart
+++ mecury_project/example/animation_challenge/lib/animation_demo/audio_screen.dart
@@ -9,44 +9,7 @@ class AudioScreen extends StatefulWidget {
 }
 
 class _AudioScreenState extends State<AudioScreen> {
-  Stopwatch stopwatch = new Stopwatch();
-
-  void stopButtonPressed() {
-    setState(() {
-      stopwatch..stop()..reset();
-    });
-  }
-
-  void rightButtonPressed() {
-    setState(() {
-      if(stopwatch.isRunning){
-        stopwatch.stop();
-      }
-      else{
-        stopwatch.start();
-      }
-    });
-  }
-
-  Widget buildButton(VoidCallback callback, Color color,
-      BuildContext context, IconData icon) {
-    Size size = MediaQuery.of(context).size;
-    return Container(
-        width: size.width * 0.5,
-        alignment: Alignment.center,
-        child: RaisedButton(
-          elevation: 0.0,
-          shape: new RoundedRectangleBorder(
-              borderRadius: new BorderRadius.circular(36.0)),
-          color: color,
-          onPressed: callback,
-          child: Container(
-            width: size.width * 0.5-80.0,
-            height: MediaQuery.of(context).size.width*0.15,
-            child: Icon(icon,color: Colors.white,size: 32.0,),
-          ),
-        ));
-  }
+  Stopwatch stopwatch = Stopwatch();
 
   @override
   Widget build(BuildContext context) {
@@ -56,38 +19,84 @@ class _AudioScreenState extends State<AudioScreen> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Container(
-                height: 100.0,
-                width: 100.0,
-                child: stopwatch.isRunning
-                    ? Center(
-                        child: SpinKitWave(
-                            color: Colors.black87.withOpacity(0.7), type: SpinKitWaveType.start),
-                      )
-                    : Image.asset("assets/recorder.png")),
-            Container(
-                height: 200.0,
-                child: new Center(
-                  child: new TimerText(stopwatch: stopwatch),
-                )),
-            Row(children: <Widget>[
-              buildButton(
-                  stopButtonPressed,
-//                Colors.orangeAccent,
-                  Colors.redAccent,
-                  context,
-                  Icons.stop),
-              buildButton(
-                  rightButtonPressed,
-//                Colors.teal.withOpacity(0.7),
-                  Colors.blueAccent,
-                  context,
-                  stopwatch.isRunning?Icons.pause:Icons.play_arrow),
-            ]),
+            _buildRecordingStatus(),
+            _buildTimerText(),
+            _buildButtonRow(context),
           ],
         ),
       ),
     );
+  }
+
+  void _stopButtonPressed() {
+    setState(() {
+      stopwatch
+        ..stop()
+        ..reset();
+    });
+  }
+
+  void _rightButtonPressed() {
+    setState(() {
+      if (stopwatch.isRunning) {
+        stopwatch.stop();
+      } else {
+        stopwatch.start();
+      }
+    });
+  }
+
+  Widget _buildTimerText() {
+    return Container(
+        height: 200.0,
+        child: Center(
+          child: TimerText(stopwatch: stopwatch),
+        ));
+  }
+
+  Widget _buildRecordingStatus() {
+    return Container(
+        height: 100.0,
+        width: 100.0,
+        child: stopwatch.isRunning
+            ? Center(
+                child: SpinKitWave(
+                    color: Colors.black87.withOpacity(0.7),
+                    type: SpinKitWaveType.start),
+              )
+            : Image.asset("assets/recorder.png"));
+  }
+
+  Widget _buildButtonRow(BuildContext context) {
+    return Row(children: <Widget>[
+      _buildButton(_stopButtonPressed, Colors.redAccent, context, Icons.stop),
+      _buildButton(_rightButtonPressed, Colors.blueAccent, context,
+          stopwatch.isRunning ? Icons.pause : Icons.play_arrow),
+    ]);
+  }
+
+  Widget _buildButton(
+      VoidCallback callback, Color color, BuildContext context, IconData icon) {
+    Size size = MediaQuery.of(context).size;
+    return Container(
+        width: size.width * 0.5,
+        alignment: Alignment.center,
+        child: RaisedButton(
+          elevation: 0.0,
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(36.0)),
+          color: color,
+          onPressed: callback,
+          child: Container(
+            width: size.width * 0.5 - 80.0,
+            height: MediaQuery.of(context).size.width * 0.15,
+            child: Icon(
+              icon,
+              color: Colors.white,
+              size: 32.0,
+            ),
+          ),
+        ));
   }
 }
 
@@ -95,7 +104,7 @@ class TimerText extends StatefulWidget {
   TimerText({this.stopwatch});
   final Stopwatch stopwatch;
 
-  TimerTextState createState() => new TimerTextState(stopwatch: stopwatch);
+  TimerTextState createState() => TimerTextState(stopwatch: stopwatch);
 }
 
 class TimerTextState extends State<TimerText> {
@@ -103,7 +112,7 @@ class TimerTextState extends State<TimerText> {
   final Stopwatch stopwatch;
 
   TimerTextState({this.stopwatch}) {
-    timer = new Timer.periodic(new Duration(milliseconds: 30), callback);
+    timer = Timer.periodic(Duration(milliseconds: 30), callback);
   }
 
   void callback(Timer timer) {
@@ -116,16 +125,39 @@ class TimerTextState extends State<TimerText> {
   Widget build(BuildContext context) {
     final double width = MediaQuery.of(context).size.width;
     final TextStyle timerTextStyle = const TextStyle(
-        fontSize: 60.0, fontFamily: "Open Sans", fontWeight: FontWeight.w300,color: Colors.black87,);
+      fontSize: 60.0,
+      fontFamily: "Open Sans",
+      fontWeight: FontWeight.w300,
+      color: Colors.black87,
+    );
     List<String> formattedTime =
         TimerTextFormatter.format(stopwatch.elapsedMilliseconds);
     return Row(
       mainAxisAlignment: MainAxisAlignment.center,
       children: <Widget>[
-      Container(child: Text("${formattedTime[0]}:", style: timerTextStyle,),width: width/4.0,),
-      Container(child: Text("${formattedTime[1]}:", style: timerTextStyle,),width: width/4.1,),
-      Container(child: Text("${formattedTime[2]}", style: timerTextStyle,),width: width/4.6,),
-    ],);
+        Container(
+          child: Text(
+            "${formattedTime[0]}:",
+            style: timerTextStyle,
+          ),
+          width: width / 4.0,
+        ),
+        Container(
+          child: Text(
+            "${formattedTime[1]}:",
+            style: timerTextStyle,
+          ),
+          width: width / 4.1,
+        ),
+        Container(
+          child: Text(
+            "${formattedTime[2]}",
+            style: timerTextStyle,
+          ),
+          width: width / 4.6,
+        ),
+      ],
+    );
   }
 }
 
@@ -139,7 +171,7 @@ class TimerTextFormatter {
     String secondsStr = (seconds % 60).toString().padLeft(2, '0');
     String hundredsStr = (hundreds % 100).toString().padLeft(2, '0');
 
-    return [minutesStr,secondsStr,hundredsStr];
+    return [minutesStr, secondsStr, hundredsStr];
 //    return "$minutesStr:$secondsStr:$hundredsStr";
   }
 }
