715a73969f5984da057c653c14af7d3cc0cf40ee
Arther Smith <arther@ArtherMBP.local>
2019-01-12 18:12:41 +0800

    fix warning for example

--- mecury_project/example/bloc_demo/lib/rxdart/blocs/bloc_provider.dart
+++ mecury_project/example/bloc_demo/lib/rxdart/blocs/bloc_provider.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:bloc_demo/rxdart/blocs/count_bloc.dart';
 
 class BlocProvider extends InheritedWidget {
-  CountBLoC bLoC = CountBLoC();
+  final CountBLoC bLoC = CountBLoC();
 
   BlocProvider({Key key, Widget child}) : super(key: key, child: child);
 
