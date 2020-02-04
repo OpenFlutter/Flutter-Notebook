055467245778f729453290466ce4e07b572a505c
Vadaski <1652219550a@gmail.com>
2020-02-04 23:41:53 +0800

    优化代码
    将 Selector 替换为简单的 Provider.of(context,listen:false),实现一个不刷新的 Provider

--- mecury_project/example/provider_example/lib/screens.dart
+++ mecury_project/example/provider_example/lib/screens.dart
@@ -150,29 +150,24 @@ class SecondScreen extends StatelessWidget {
 class GoodsListScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final provider = Provider.of<GoodsListProvider>(context, listen: false);
     return ChangeNotifierProvider(
       create: (_) => GoodsListProvider(),
-      child: Selector<GoodsListProvider, GoodsListProvider>(
-        shouldRebuild: (pre, next) => false,
-        selector: (context, provider) => provider,
-        builder: (context, provider, child) {
-          return ListView.builder(
-            itemCount: provider.total,
-            itemBuilder: (context, index) {
-              return Selector<GoodsListProvider, Goods>(
-                selector: (context, provider) => provider.goodsList[index],
-                builder: (context, data, child) {
-                  print(('No.${index + 1} rebuild'));
+      child: ListView.builder(
+        itemCount: provider.total,
+        itemBuilder: (context, index) {
+          return Selector<GoodsListProvider, Goods>(
+            selector: (context, provider) => provider.goodsList[index],
+            builder: (context, data, child) {
+              print(('No.${index + 1} rebuild'));
 
-                  return ListTile(
-                    title: Text(data.goodsName),
-                    trailing: GestureDetector(
-                      onTap: () => provider.collect(index),
-                      child: Icon(
-                          data.isCollection ? Icons.star : Icons.star_border),
-                    ),
-                  );
-                },
+              return ListTile(
+                title: Text(data.goodsName),
+                trailing: GestureDetector(
+                  onTap: () => provider.collect(index),
+                  child:
+                      Icon(data.isCollection ? Icons.star : Icons.star_border),
+                ),
               );
             },
           );
